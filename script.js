43c9ab415656447ce24534c2271f664bcd1a7b12
Deeprasai6 <poonyapat6006@gmail.com>
2023-10-11 10:47:18 +0700

    update

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// alert("Hello weclome to mycv");
+alert("Hello weclome to mycv");
+
+
+// กด Soft skills แล้วจะเลื่อยลงมาว่ามีอะไรบ้าง
+// document.getElementById("slip").onclick = function() {slip()};
+function softskills() {
+    if (document.getElementById("softskills").innerHTML === "Soft skills") {
+        document.getElementById("softskills").innerHTML = "Soft skills<br>-dfdfdfd<br>-fghghghg";
+    }
+    else {
+        document.getElementById("softskills").innerHTML = "Soft skills";
+    }
+}
+
+
+function hardskills() {
+    if (document.getElementById("hardskills").innerHTML === "Hard skills") {
+        document.getElementById("hardskills").innerHTML = "Hard skills<br>-dfdfdfd<br>-fghghghg";
+    }
+    else {
+        document.getElementById("hardskills").innerHTML = "Hard skills";
+    }
+}
