6e019fec5e8bf996cf617fe96cbd6726c9dd241f
Deeprasai6 <poonyapat6006@gmail.com>
2023-10-18 01:48:20 +0700

    up buttonlike

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 alert("Hello weclome to mycv");
-
+let counter = 0;
 
 // กด Soft skills แล้วจะเลื่อยลงมาว่ามีอะไรบ้าง
 // กด Hard skills แล้วจะเลื่อยลงมาว่ามีอะไรบ้าง
 // document.getElementById("slip").onclick = function() {slip()};
 function softskills() {
     if (document.getElementById("softskills").innerHTML === "Soft skills") {
-        document.getElementById("softskills").innerHTML = "Soft skills<br>-dfdfdfd<br>-fghghghg";
+        document.getElementById("softskills").innerHTML = "Soft skills<br>-Time management<br>-Teamwork<br>-Very sociable";
     }
     else {
         document.getElementById("softskills").innerHTML = "Soft skills";
@@ -16,9 +16,20 @@ function softskills() {
 
 function hardskills() {
     if (document.getElementById("hardskills").innerHTML === "Hard skills") {
-        document.getElementById("hardskills").innerHTML = "Hard skills<br>-dfdfdfd<br>-fghghghg";
+        document.getElementById("hardskills").innerHTML = "Hard skills<br>-PLC<br>-HTML,Css,Js<br>-Process control";
     }
     else {
         document.getElementById("hardskills").innerHTML = "Hard skills";
     }
 }
+
+function count() {
+    counter++;
+    document.querySelector('h3').innerHTML = counter;
+    if (counter % 1 === 0) {
+        alert(`Thank you for liking my page 💖`);
+    }
+}
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('button').onclick = count;
+});
